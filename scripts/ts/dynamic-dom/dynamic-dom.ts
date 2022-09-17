6c075ce45821005124701b6895af8412dd7fa4a3
ariellerosenblatt <ariellerosenblatt@wustl.edu>
2022-09-17 12:39:35 -0500

    studio

--- scripts/ts/dynamic-dom/dynamic-dom.ts
+++ scripts/ts/dynamic-dom/dynamic-dom.ts
@@ -3,12 +3,37 @@ import { Accordion } from './accordion';
 import { doSomething } from './do-something';
 import { HTMLContent, itemsToCache } from './html-imports';
 import { Slideshow } from './slideshow';
+// import { setupSticky } from './sticky';
 
 // Put all function calls that need to be made on every page load inside the setupAll function body.
 export function PutStudentPageLoadOperationsInsideThisStudentBody() {
     // TODO: Put all operations that you want to happen on ever page load in this function.
     // For example you could write: Sticky.setup()
     doSomething();
+    setupSticky();
+}
+console.log("HI");
+export function setupSticky()
+{
+     console.log("2")
+    // When the user scrolls the page, execute myFunction 
+    window.onscroll = function() {setupSticky()};
+    
+    // Get the header
+    var header = document.getElementById('main-menu-container');
+    console.log(header);
+    
+    // Get the offset position of the navbar
+    var sticky = header.offsetTop;
+    
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function setupSticky() {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
 }
 
 export async function setupAll() {
@@ -28,3 +53,5 @@ itemsToCache.forEach((item: HTMLContent) => {
 console.log('dynamic-dom loaded');
 // Do not touch this line, needed to reinitialize code in the dynamic-dom.ts setupAll function
 window.addEventListener('newPageLoad', () => setupAll());
+
+
